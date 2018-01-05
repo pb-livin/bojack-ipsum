52a97e1556809fc3519f2a0d452f57ddb878b60b
Gabbie <gabbie.metheny@gmail.com>
2018-01-05 15:05:25 -0800

    refactor functions and getJSON

--- js/app.js
+++ js/app.js
@@ -6,47 +6,62 @@ const $results = $('#results');
 const $ipsum = $('#ipsum');
 const $reload = $('#reload');
 
+// return an array of x unique random numbers in a given range
+function randomNumberArray(x, range) {
+  randomArray = [];
+  let count = 0;
+  while (count < x) {
+    const randomChoice = Math.floor(Math.random() * range);
+    // check to see if number is already in array before adding
+    if (randomArray.indexOf(randomChoice) === -1) {
+      randomArray.push(randomChoice);
+      count += 1;
+    } // end if statement
+  } // end while loop
+  return randomArray;
+} // end randomNumberArray()
+
 $results.hide();
-$getIpsum.on('click', () => {
-  // capture user input from select menu
-  const numberOfParagraphs = $paragraphs.val();
+
+$getIpsum.click(() => {
   // hide user input div
   $userInput.fadeOut(300);
   $tagline.fadeOut(300);
-  // jQuery AJAX method
-  $.getJSON('https://pb-livin.github.io/bojack-ipsum/data/quotes.json', (response) => {
-    // concatenate all arrays in json response
+
+  // capture user input from select menu
+  const $numberOfParagraphs = $paragraphs.val();
+  const quotesURL = 'https://pb-livin.github.io/bojack-ipsum/data/quotes.json';
+
+  function makeParagraphs(quotesObject) {
+    // concatenate all arrays in json quotes object
     let ipsumArray = [];
-    for (array in response) {
-      ipsumArray = ipsumArray.concat(response[array]);
+    for (array in quotesObject) {
+      ipsumArray = ipsumArray.concat(quotesObject[array]);
     }
-    // return an array of unique random numbers
-    function randomP(num) {
-      randomArray = [];
-      let count = 0;
-      while (count < num) {
-        const randomChoice = Math.floor(Math.random() * ipsumArray.length);
-        if (randomArray.indexOf(randomChoice) === -1) {
-          randomArray.push(randomChoice);
-          count += 1;
-        } // end if statement
-      } // end while loop
-      return randomArray;
-    } // end randomP()
-    // call randomP() with user input, store in indexes
-    const indexes = randomP(numberOfParagraphs);
+
+    // call randomP() with user input and ipsum array length, store in indexes
+    const indexes = randomNumberArray($numberOfParagraphs, ipsumArray.length);
+
     // loop over indexes and add paragraphs from ipsumArray at each index listed in indexes
-    for (let i = 0; i < indexes.length; i += 1) {
-      const p = document.createElement('p');
-      const index = indexes[i];
-      p.textContent = ipsumArray[index];
-      $ipsum.append(p);
-    } // end for loop
+    $.each(indexes, (i, indexValue) => {
+      const $p = $('<p>').text(ipsumArray[indexValue]);
+      $ipsum.append($p);
+    });
+
+    // display ipsum results
     $results.delay(400).fadeIn(500);
-  }).fail((jqXHR) => {
-    $('#ipsum').html(`<p>Bummer! Error ${jqXHR.status}: ${jqXHR.statusText}. Refresh your browser to try again.</p>`);
+  }
+
+  // jQuery AJAX method
+  $.getJSON(quotesURL, makeParagraphs)
+  // jQuery AJAX fail
+  .fail((jqXHR) => {
+    $results.html(`<p>Bummer! Error ${jqXHR.status}: ${jqXHR.statusText}.</p>`);
+    $results.fadeIn(500); // right now, getting 'Error 0: error' when it should be 'Error 404: Not Found'
   }); // end getJSON/fail
 }); // end click listener
+
+// reloads the page to get new ipsum
 $reload.click(() => {
   location.reload();
 });
