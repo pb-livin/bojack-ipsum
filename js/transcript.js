9e16a74152a35ee04ef732651739f100bddc6d53
Gabbie <gabbie.metheny@gmail.com>
2018-01-19 16:16:27 -0800

    functional version of generator from transcript

--- js/transcript.js
+++ js/transcript.js
@@ -1,23 +1,76 @@
 'use strict';
 
-// having some problems with fs/require in the browser (node syntax): just use an ajax call...? but how? need to figure out how make this accessible to the app
-// const fs = require('fs');
-// getting a better console error now, but still need to figure out how to make the browser happy: full/absolute path to fs? Can't find one...
-import fs from 'fs';
+function randomQuote(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
-const output = fs.readFileSync('data/transcript-1.txt', 'UTF-8')
-  .split('\n')
-  .map(line => line.trim('\r'))
-  .filter(line => line.length > 0)
-  .map(line => line.split(': '))
-  .filter(line => line.length > 1)
-  .reduce((script, line) => {
-    script[line[0]] = script[line[0]] || [];
-    script[line[0]].push(line[1]);
-    return script;
-  }, {})
+function makeParagraph(lines, array) {
+  let paragraphText = '';
+  for (let i = 0; i < lines; i++) {
+    paragraphText += `${randomQuote(array)} `;
+  }
+  return paragraphText;
+}
+
+function makeIpsum(paragraphs, lines, array) {
+  let paragraphArray = [];
+  for (let i = 0; i < paragraphs; i++) {
+    paragraphArray.push(makeParagraph(lines, array));
+  }
+  return paragraphArray;
+}
+
+function appendIpsumToDOM(element, paragraphs, lines, array) {
+  let paragraphArray = makeIpsum(paragraphs, lines, array);
+  for (let i = 0; i < paragraphArray.length; i++) {
+    $(element).append($('<p>').text(paragraphArray[i]));
+  }
+}
+
+const output = fetch('data/transcript-1.txt', {
+  method: 'GET',
+  'content-type': "text/plain"
+})
+  .then(response => response.text())
+  .then(text => {
+    text = text
+      // splits on newlines between quotes
+      .split(/\n\r\n/)
+      // trims carriage returns
+      .map(line => line.trim('/r'))
+      // replaces newlines within quotes with a space
+      .map(line => line.replace(/\n/g, ' '))
+      // removes empty lines
+      .filter(line => line.length > 0)
+      // removes stage directions in [ ], add ( )?
+      .map(line => line.replace(/\[.*\]/g, ''))
+      // removes whitespace at either end of a line
+      .map(line => line.trim())
+      // splits lines at : and one or more spaces
+      .map(line => line.split(/:\s+/))
+      // filters out single string arrays
+      .filter(line => line.length > 1)
+      .reduce( (transcript, line) => {
+        // ensures ENT, " ENT", etc. are not included
+        // and that there is content in the quote
+        // before adding to character array
+        if (!/^[\s"]*ENT[\s"]*$/.test(line[0]) && line[1].length>0) {
+          // accounts for quotes that were split on ": "
+          if (line.length > 2) {
+            line[1] += `: ${line[2]}`;
+          }
+          // adds quote to character array, creating it if it doesn't already exist
+          transcript[line[0]] = transcript[line[0]] || [];
+          transcript[line[0]].push(line[1]);
+        }
+        return transcript;
+      }, {});
+    return text;
+  }).then(text => {
+    let flattened = [];
+    for (let array in text) {
+      flattened = flattened.concat(text[array]);
+    }
+    appendIpsumToDOM('header', 5, 10, flattened);
+  })
 ;
-
-// console.log(output);
-const bojackQuotes = output.BOJACK;
-console.log(bojackQuotes);
