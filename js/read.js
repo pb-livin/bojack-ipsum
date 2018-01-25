e021c99b6e6b82e7570178c26f15675a692892a0
Gabbie <gabbie.metheny@gmail.com>
2018-01-24 17:29:09 -0800

    initial commit

---
+++ js/read.js
@@ -0,0 +1,33 @@
+function makeTranscript() {
+  const fs = require('fs');
+  const filepath = process.argv.slice(2);
+  filepath.forEach((file) => {
+    const textFile = file
+                     .replace('xml', 'txt')
+                     .replace(/.+\//g, '');
+
+    fs.readFile(file, 'utf-8', (err, data) => {
+      data = data
+        .replace(/<br\/?>/g, ' ')
+        .replace(/<\/?span[^<>]*>/g, '')
+        .replace(/<p begin="/g, '')
+        .replace(/"\send=[^<>]+>/g, '\n')
+        .replace(/<\/p[^<>]*>/g, '\n')
+        .replace(/<[^<>]+>\n/g, '')
+        .replace(/\d+t/g, (match) => {
+          // const newTime = '3766347743t';
+          const captionTime = new Date(null);
+          captionTime.setMilliseconds(parseInt(match)/10000);
+          return captionTime.toISOString().substr(14,5);
+        });
+      fs.writeFile(textFile, data, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(`parsed ${textFile} from ${file}`);
+      });
+    });
+  });
+}
+
+makeTranscript();
